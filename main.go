62942ab3d3362fa43d04974e70625d6445a60b8a
Vladislav Kovun <vkovun@phoenixit.ru>
2018-09-17 17:49:40 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	sdl "GoSdlWrapper/sdl_wrapper"
+	"time"
+)
+
+func main() {
+
+	sdl.Init("SDL WRAPPER TEST", 320, 200)
+
+	defer sdl.Defer_me()
+
+	sdl.Clear()
+
+	sdl.DrawCircle(75, 75, 50)
+
+	sdl.Flush()
+
+	time.Sleep(5000 * time.Millisecond)
+
+	//
+	//surface.FillRect(nil, 0)
+	//
+	//rect := sdl.Rect{0, 0, 200, 200}
+	//surface.FillRect(&rect, 0xffff0000)
+	//window.UpdateSurface()
+	//
+	//running := true
+	//var x, y, w, h int32
+	//
+	//for running {
+	//	x++
+	//	y++
+	//	w++
+	//	h++
+	//	surface.FillRect(nil, 0)
+	//
+	//
+	//	rect := sdl.Rect{x, y, w, h}
+	//	surface.FillRect(&rect, 0xffff0000)
+	//	window.UpdateSurface()
+	//	time.Sleep(200 * time.Millisecond)
+	//
+	//	for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+	//		switch event.(type) {
+	//		case *sdl.QuitEvent:
+	//			println("Quit")
+	//			running = false
+	//			break
+	//		}
+	//	}
+	//}
+}
---
+++ sdl_wrapper/sdl_wrapper.go
@@ -0,0 +1,94 @@
+package sdl_wrapper
+
+import (
+	"fmt"
+	"github.com/veandco/go-sdl2/sdl"
+	"os"
+)
+
+var (
+	window sdl.Window
+	// surface *sdl.Surface
+	renderer *sdl.Renderer
+)
+
+// system funcs
+
+func Init(title string, w, h int32) {
+
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+
+	window, err := sdl.CreateWindow(title, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		w, h, sdl.WINDOW_SHOWN)
+
+	//surface, err = window.GetSurface()
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	renderer, err = sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create renderer: %s\n", err)
+		panic(err)
+	}
+
+}
+
+func Defer_me() {
+	sdl.Quit()
+	window.Destroy()
+	renderer.Destroy()
+}
+
+// clear/flush funcs
+
+func Flush() {
+	// window.UpdateSurface()
+	renderer.Present()
+}
+
+func Clear() {
+	renderer.Clear()
+}
+
+// draw funcs
+
+func DrawLine(x, y, x1, y1 int32){
+	renderer.DrawLine(x, y, x1, y1)
+}
+
+func DrawCircle(x0, y0, radius int32) {
+	x := radius-1
+	var (
+		y, dx, dy int32
+	)
+	y = 0
+	dx = 1
+	dy = 1
+	err := dx - (radius << 1)
+
+	for x >= y {
+		renderer.DrawPoint(x0 + x, y0 + y)
+		renderer.DrawPoint(x0 + y, y0 + x)
+		renderer.DrawPoint(x0 - y, y0 + x)
+		renderer.DrawPoint(x0 - x, y0 + y)
+		renderer.DrawPoint(x0 - x, y0 - y)
+		renderer.DrawPoint(x0 - y, y0 - x)
+		renderer.DrawPoint(x0 + y, y0 - x)
+		renderer.DrawPoint(x0 + x, y0 - y)
+
+		if err <= 0 {
+			y++
+			err += dy
+			dy += 2
+		}
+
+		if err > 0 {
+			x--
+			dx += 2
+			err += dx - (radius << 1)
+		}
+	}
+}
